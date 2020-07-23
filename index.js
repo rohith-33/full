322da5d806ad7b5a6bb0db8a9def83736cc53a67
rohith-33 <rohithmuthyala3@gmail.com>
2020-07-23 17:05:23 +0530

    Just a Change

---
+++ index.js
@@ -0,0 +1,9 @@
+var express=require('express');
+var app=express();
+app.get('/',function(req,res){
+    res.send('Hello world');
+})
+var port=3000;
+app.listen(port,function(){
+     console.log("Site running on http://localhost:"+port);
+});
