5df40499099ebea588d115808807957d5caef33f
rohith-33 <rohithmuthyala3@gmail.com>
2020-07-26 17:20:17 +0530

    Adding html page to be served

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express=require('express');
 var app=express();
 app.get('/',function(req,res){
-    res.send('Hello world');
+    //res.send('Hello world');
+    res.sendFile(__dirname+'/frontend/html/index.html');
 })
 var port=process.env.PORT || 3000 ;
 app.listen(port,function(){
