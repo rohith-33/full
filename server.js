a36ec7f433f56389913cca7899bbf1813718d4e7
rohith-33 <rohithmuthyala3@gmail.com>
2020-07-28 13:03:02 +0530

    cass

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.use('/',function(req,res,next){
     console.log("yeah i got request first,but i am not handling it");
      next();
 })
+app.get('/Text-Tages',function(req,res){
+    //res.send('Hello world');
+    res.sendFile(__dirname+'/frontend/html/TextTages.html');
+})
 app.get('/',function(req,res){
     //res.send('Hello world');
     res.sendFile(__dirname+'/frontend/html/index.html');
