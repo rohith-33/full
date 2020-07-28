c8087f2ca043d19d589fc9261fe31706a5894733
rohith-33 <rohithmuthyala3@gmail.com>
2020-07-28 12:41:42 +0530

    change

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express=require('express');
 var app=express();
-//app.use(express.static(__dirname+'/frontend'));
+app.use(express.static(__dirname+'/frontend'));
+app.use('/',function(req,res,next){
+    console.log("yeah i got request first,but i am not handling it");
+     next();
+})
 app.get('/',function(req,res){
     //res.send('Hello world');
     res.sendFile(__dirname+'/frontend/html/index.html');
